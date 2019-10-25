4a9ef2014cd3d1aa4f1ca6d5fd7c6b375a0dd112
Jared York <jared.york@jaredyork.com>
2019-10-24 22:54:29 -0400

    Converted the main plugin file to an object-oriented approach.

--- wp-performant-media.php
+++ wp-performant-media.php
@@ -12,17 +12,26 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-add_action('wp_enqueue_scripts', function () {
-    wp_enqueue_script( 'wp-performant-media.js', plugins_url( 'dist/wp-performant-media.js',  __FILE__ ), [], '1.0.0', true );
-    wp_enqueue_style( 'wp-performant-media.css', plugins_url( 'dist/wp-performant-media.css',  __FILE__ ), [], '1.0.0' );
-});
+class WP_Performant_Media {
+	public function __construct() {
 
-add_filter('the_content', function ($content) {
-	$content = preg_replace( "/<img(.*?)(src=|srcset=)(.*?)>/i", '<img$1data-$2$3>', $content );
-	$content = preg_replace( '/<img(.*?)class=\"(.*?)\"(.*?)>/i', '<img$1class="$2 lazy-load"$3>', $content );
-	$content = preg_replace( '/<img((?:(?!class=).)*?)>/i', '<img class="lazy-load"$1>', $content);
-	return $content;
-});
+		add_action( 'wp_enqueue_scripts', array( $this, "load_scripts" ) );
 
+		add_filter( 'the_content', array( $this, "lazyloader_filter" ) );
+		
+	}
 
+	public function load_scripts() {
+		wp_enqueue_script( 'wp-performant-media.js', plugins_url( 'dist/wp-performant-media.js',  __FILE__ ), [], '1.0.0', true );
+		wp_enqueue_style( 'wp-performant-media.css', plugins_url( 'dist/wp-performant-media.css',  __FILE__ ), [], '1.0.0' );
+	}
 
+	public function lazyloader_filter( $content ) {
+		$content = preg_replace( "/<img(.*?)(src=|srcset=)(.*?)>/i", '<img$1data-$2$3>', $content );
+		$content = preg_replace( '/<img(.*?)class=\"(.*?)\"(.*?)>/i', '<img$1class="$2 lazy-load"$3>', $content );
+		$content = preg_replace( '/<img((?:(?!class=).)*?)>/i', '<img class="lazy-load"$1>', $content);
+		return $content;
+	}
+}
+
+new WP_Performant_Media();
